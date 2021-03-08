6d1973186817f902ba6391c4fb8abfbd7cfb66a6
Celscius <60767284+Celscius@users.noreply.github.com>
2021-03-08 14:33:15 +0700

    delete api

--- class/Simple-jQuery-Sorting-Filtering-Plugin-simpleSort/README.md
+++
@@ -1,4 +0,0 @@
-# simpleSort 
-Very simple Javascript/jQuery script for sorting &amp; filtering elements inside a parent container
-
-[Demo](http://amadreason.github.io/simpleSort/)
--- class/api/twitteroauth-main/key/user.php
+++
@@ -1,5 +0,0 @@
-	<?php	
-		$CONSUMER_KEY ='9WAy69DDou6HIpfWF6M7RSNXO';
-		$CONSUMER_SECRET ='m7hVeZgqfcaVZs8Vkdhs5nMPIWvNhg3uMJLWzMTEVU1EuvRJSW';
-		$access_token = '158585527-6go1GKrgSkOiD4HDieBdMnRmdL2neGyBzihXCyn0';
-		$access_token_secret ='53F4yWyonM1g4wHjoI2kAOCprkwpnofHisU1tOiTeV1g9';
