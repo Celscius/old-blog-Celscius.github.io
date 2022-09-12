bfc2dcc473a745c7fb356e86d35b59ce3415a4fe
Celscius <60767284+Celscius@users.noreply.github.com>
2022-09-12 21:26:19 +0800

    posts

---
+++ app/single/body.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   	$('#loader').load('loader.html');
+   	$('#left-sidebar').load('sidebar-left.html');  
+   	$('#blog-title').load('blog-title.html');
+   	$('#blog-post').load('blog-post.html')  
+   	$('#subscribe').load('subscribe.html'); 
+   	$('#footer').load('footer.html');
+   	$('#right-sidebar').load('sidebar-right.html');
+   	$('#back-to-start').load('back.html');      
+});
+                          
